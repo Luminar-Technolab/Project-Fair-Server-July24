cbe5ae907494df637fb9619a14d5ae60ad13d9bc
soniagkalliath <soniageorge111@gmail.com>
2024-12-05 12:47:55 +0530

    project add

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const userController =  require('../controllers/userController')
+const projectController = require('../controllers/projectController')
+const jwtMiddleware = require('../middlewares/jwtMiddleware')
+const multerMiddleware = require('../middlewares/multerMiddleware')
 
 const router = new express.Router()
 
@@ -7,6 +10,7 @@ const router = new express.Router()
 router.post('/register',userController.registerController)
 //login - post
 router.post('/login',userController.loginController)
-
+//add-project - post
+router.post('/add-project',jwtMiddleware,multerMiddleware.single('projectImage'),projectController.addProjectController)
 
 module.exports = router
